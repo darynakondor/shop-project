b4acc2d653f2d9987733bdb6ea6da7c6b4c5d122
Daryna <condordaryna@gmail.com>
2022-08-01 16:10:03 +0300

    add slider and modal

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
 const  productsCountEl = document.getElementById("products-count");
 const addToCartBtns = document.querySelectorAll(".btn-cart");
 let count = 0;
-
 addToCartBtns.forEach((item) =>
     item.addEventListener("click", function () {
         count += 1;
-        productsCountEl.innerHTML = count;
+        productsCountEl.textContent = count;
     })
 );
 
 
 
-const buttonLike = document.querySelectorAll(".icon-like");
-const iconLike = document.querySelectorAll(".like");
-buttonLike.forEach((item) =>
-    item.addEventListener("click", function () {
-        let colorBlue = '#2c71b8';
-        item.style.background = colorBlue;
+let buttonLike = document.querySelectorAll(".icon-like");
+buttonLike.forEach((item) => {
+    item.addEventListener("click", function(e) {
+        item.classList.toggle("liked");
+        item.style.backgroundColor = "#2c71b8";
     })
-);
+})
 
-iconLike.forEach((item) =>
-    item.addEventListener("click", function() {
-        let imgIconLike = 'url("image/icon-like-white.png")';
-        item.style.backgroundImage = imgIconLike;
-    })
-);
+
+
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".btn-close");
+
+window.addEventListener('scroll', function(e) {
+  if (window.scrollY >= (document.documentElement.scrollHeight - document.documentElement.clientHeight)*0.5) {
+    window.requestAnimationFrame(function() {
+        modal.classList.add('show');
+    });
+  }
+});
+
+closeBtn.addEventListener("click", function() {
+    modal.classList.add("hide");
+});
+
+
+$(".slider-block").slick({
+    autoplay:true,
+    dots:true
+})
+
 
 
