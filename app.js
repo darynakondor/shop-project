f54c31f191d02696a9432ada1e54d0a04b0e1be0
Daryna <condordaryna@gmail.com>
2022-08-03 13:29:13 +0300

    add counter to products

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 const  productsCountEl = document.getElementById("products-count");
 const addToCartBtns = document.querySelectorAll(".btn-cart");
-let count = 0;
 addToCartBtns.forEach((item) =>
     item.addEventListener("click", function () {
-        count += 1;
-        productsCountEl.textContent = count;
+        productsCountEl.textContent++;
     })
 );
 
 
 let buttonLike = document.querySelectorAll(".icon-like");
 buttonLike.forEach((item) => {
-    item.addEventListener("click", function(e) {
+    item.addEventListener("click", function() {
         item.classList.toggle("liked");
-        item.style.backgroundColor = "#2c71b8";
+        item.classList.toggle("bg-color")
     })
 })
-
 
 
 let modal = document.querySelector(".modal");
 let closeBtn = document.querySelector(".btn-close");
 
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', function() {
   if (window.scrollY >= (document.documentElement.scrollHeight - document.documentElement.clientHeight)*0.5) {
     window.requestAnimationFrame(function() {
         modal.classList.add('show');
@@ -34,11 +31,66 @@ closeBtn.addEventListener("click", function() {
     modal.classList.add("hide");
 });
 
+modal.addEventListener("click", function() {
+    modal.classList.add("hide");
+});
+
 
 $(".slider-block").slick({
     autoplay:true,
     dots:true
 })
+
+
+function amountProducts(count) {
+    let btnIncrement = count.querySelector(".btn-increment");
+    let btnDecrement = count.querySelector(".btn-decrement");
+    let inputValue = count.querySelector(".input-value");
+    
+    btnIncrement.addEventListener("click", function() {
+        btnDecrement.disabled = false;
+        inputValue.value++; 
+        btnDecrement.classList.remove("disable");
+        if (inputValue.value >= 5) {
+            this.disabled = true;
+            this.classList.add("disable");
+        }
+    });
+
+    btnDecrement.addEventListener("click", function() {
+        btnIncrement.disabled = false;
+        btnIncrement.classList.remove("disable");
+        this.disabled = true;
+        this.classList.add("disable");
+        
+        if (inputValue.value > 1) {
+            this.disabled = false;
+            inputValue.value--;
+            this.classList.remove("disable");
+        }
+    })
+}
+
+let productQuantity = document.querySelectorAll(".product-quantity");
+productQuantity.forEach(amountProducts);
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
 
 
 
